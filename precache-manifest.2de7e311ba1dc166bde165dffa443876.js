e50e50aba2a493ba3dc9d02b00a6b33fae3701cd
geo <gbanoaol@gmail.com>
2020-07-20 16:06:08 -0400

    Updates

---
+++ precache-manifest.2de7e311ba1dc166bde165dffa443876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0edbd13f9db0f10222d2ab135fbbfab",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "df098b015e811c92bdee",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "772d388e9dde147e369f",
+    "url": "/robofriends/static/js/2.d3d8e067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d3d8e067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df098b015e811c92bdee",
+    "url": "/robofriends/static/js/main.53c1b450.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
